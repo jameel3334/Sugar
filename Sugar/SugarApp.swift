dbb2ee2e797567443bb68b53b3b368a950c1e432
Mohammed Jameeluddin <jameel3334@gmail.com>
2023-11-07 09:10:39 -0500

    Initial Commit

---
+++ Sugar/SugarApp.swift
@@ -0,0 +1,17 @@
+//
+//  SugarApp.swift
+//  Sugar
+//
+//  Created by Mohammed Jameeluddin on 11/7/23.
+//
+
+import SwiftUI
+
+@main
+struct SugarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
