e0555ddc0e8ec1ffabf8203a3a2edd95aec2d1ef
Mohammed Jameeluddin <jameel3334@gmail.com>
2023-11-07 20:28:23 -0500

    Models

---
+++ Sugar/Model/Dessert.swift
@@ -0,0 +1,28 @@
+//
+//  Dessert.swift
+//  Sugar
+//
+//  Created by Mohammed Jameeluddin on 11/7/23.
+//
+
+import Foundation
+
+struct Desserts: Codable {
+    let desserts: [Dessert]
+    
+    enum CodingKeys: String, CodingKey {
+        case desserts = "meals"
+    }
+}
+
+struct Dessert: Codable, Identifiable, Equatable {
+    let title: String
+    let image: String
+    let id: String
+    
+    enum CodingKeys: String, CodingKey {
+        case title = "strMeal"
+        case image = "strMealThumb"
+        case id    = "idMeal"
+    }
+}
